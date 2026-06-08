fe403f015661df765f8338d571a5294edde35e14
Francis <108684301+g4m3m4g@users.noreply.github.com>
2026-06-08 22:10:49 +0700

    ✨ feat: add dashboard page with CRUD functionality

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
-// import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  //   { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: '**', redirectTo: 'login' },
 ];
